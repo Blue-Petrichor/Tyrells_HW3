8edb453cf544a8ac5cea9537cf7f376e727e1711
Michael Shaw <Michaelshaw3@mail.weber.edu>
2017-02-10 20:29:14 +0000

    Added Tyrells_hw3.sh file

---
+++ Tyrells.hw3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: Tyrells.hw3.sh
+# 
+#         USAGE: ./Tyrells.hw3.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Michael Shaw (), Michaelshaw3@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 02/10/2017 20:27
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+#needs to add files
+exit 0
+
